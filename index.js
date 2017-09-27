0c276f789ff34bf75b376b0b86e464995e1d0a84
szaib1986 <muhammad.jahanzeb@confiz.com>
2017-09-27 16:47:10 +0500

    cluster not working

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-console.log('Application starting...');
+
 const express = require('express'),
 	cluster = require('cluster'),
 	config = require('./configuration');
-
-if (cluster.isMaster){
-	const numWorkers = require('os').cpus().length;
-
-	for (let i = 0; i < numWorkers; i++)
-		cluster.fork();
-
-	cluster.on('online', function(worker) {
-        console.log('Worker ' + worker.process.pid + ' is online');
-    });
-
-    cluster.on('exit', function(worker, code, signal) {
-        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-        console.log('Starting a new worker');
-        cluster.fork();
-    });
-}
-else {
 
 	const app = new express();
 
@@ -32,7 +14,6 @@ else {
 	});
 	
 	app.listen(config.port, () => {
-		console.log('Magic happening at 3000');
+		console.log('Magic happening at ', config.port);
 	});
 
-}
