18b744992386d422e25895009aecc0cc609433a7
szaib1986 <muhammad.jahanzeb@confiz.com>
2017-09-27 20:02:48 +0500

    port error issue - fixed

--- configuration.js
+++ configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 8080,
 	secretTokenKey: '$pabcde#&;:[]plmn|.,q6h5d#67ajmdnue^ipoie9*',
 	secretEncryptionKey: '(ps(lols554s#21|[]!bcbvzz5d4w54f3s1f5e4f3sfd@$%'
 };
